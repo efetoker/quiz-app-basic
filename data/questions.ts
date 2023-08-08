3629acb6b2b0e6d83f01f2cd8cad7f6c17408bca
Efe TOKER <efetoker@gmail.com>
2023-08-08 18:50:47 +0300

    questions added

---
+++ data/questions.ts
@@ -0,0 +1,28 @@
+const questions = [
+    {
+      id: 1,
+      question: "What is the process of converting analog signals into digital data called?",
+      options: ["Encoding", "Decoding", "Encryption", "Modulation"],
+      answer: "Decoding"
+    },
+    {
+        id: 2,
+        question: "What is the smallest unit of digital information?",
+        options: ["Byte", "Bit", "Megabyte", "Gigabyte"],
+        answer: "Bit"
+    },
+    {
+        id: 3,
+        question: "Which programming language is used for web development?",
+        options: ["PHP", "Ruby", "Swift", "HTML"],
+        answer: "HTML"
+    },
+    {
+        id: 4,
+        question: "What is the standard file format for images on the web?",
+        options: ["PNG", "PDF", "TXT", "DOC"],
+        answer: "PNG"
+    },
+  ];
+  
+  export default questions;
